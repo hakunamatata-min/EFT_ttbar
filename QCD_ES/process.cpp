4e06db09f1dc498d85bdaf0ddf953f2c92d3691b
Yuekai-Song <songyk2020@gmail.com>
2023-08-28 08:16:02 +0800

    fixed a bug

--- QCD_ES/process.cpp
+++ QCD_ES/process.cpp
@@ -2,9 +2,9 @@
 void process(TString outdir, TString outputFile, TString input, int year, int cate){
     int num_j, num_e, num_m, num_g = 0;
     int type = 0;
-    if(outputFile.Contains("EMEnriched") || outputFile.Contains("EGamma") || outputFile.Contains("SingleElectron") || outputFile.Contains("DoubleEG"))
+    if(outputFile.Contains("EGamma") || outputFile.Contains("SingleElectron") || outputFile.Contains("DoubleEG"))
         type = 1;
-    else if(outputFile.Contains("MuEnrichedPt5") || outputFile.Contains("SingleMuon"))
+    else if(outputFile.Contains("SingleMuon"))
         type = 2;
     read_object r(input, !bool(type));
     select_tree *s;
